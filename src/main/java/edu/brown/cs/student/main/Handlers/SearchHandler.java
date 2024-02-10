23327f1772d8413058d1f125f14231a3dc013b52
Asia Nguyen <asia_nguyen@brown.edu>
2024-02-10 18:59:28 -0500

    feat: search handler started

--- src/main/java/edu/brown/cs/student/main/Handlers/SearchHandler.java
+++ src/main/java/edu/brown/cs/student/main/Handlers/SearchHandler.java
@@ -1,5 +1,27 @@
 package edu.brown.cs.student.main.Handlers;
 
-public class SearchHandler {
+import edu.brown.cs.student.main.Searcher;
+import edu.brown.cs.student.main.Utility;
+import java.util.HashMap;
+import java.util.Map;
+import spark.Request;
+import spark.Response;
+import spark.Route;
 
+public class SearchHandler implements Route {
+  private Utility utility;
+
+//main = Server class, which will make instances of handlers
+  //search handler make instance of utility
+  public SearchHandler() {
+
+  }
+  @Override
+  public Object handle(Request request, Response response) throws Exception {
+    String value = request.queryParams("value");
+    String header = request.queryParams("header");
+    Map<String, Object> responseMap = new HashMap<>();
+
+    return null;
+  }
 }
