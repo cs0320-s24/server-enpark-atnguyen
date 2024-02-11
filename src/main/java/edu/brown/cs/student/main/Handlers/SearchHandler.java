7e073fc65673884aca37a6ac3b04e830a3257f1e
Asia Nguyen <asia_nguyen@brown.edu>
2024-02-11 15:45:15 -0500

    chore: merge handler clases from local to main

--- src/main/java/edu/brown/cs/student/main/Handlers/SearchHandler.java
+++ src/main/java/edu/brown/cs/student/main/Handlers/SearchHandler.java
@@ -1,7 +1,6 @@
 package edu.brown.cs.student.main.Handlers;
 
-import edu.brown.cs.student.main.Searcher;
-import edu.brown.cs.student.main.Utility;
+import edu.brown.cs.student.main.SearchCSV.Searcher;
 import java.util.HashMap;
 import java.util.Map;
 import spark.Request;
@@ -9,10 +8,10 @@ import spark.Response;
 import spark.Route;
 
 public class SearchHandler implements Route {
-  private Utility utility;
+  private Searcher searcher;
 
 //main = Server class, which will make instances of handlers
-  //search handler make instance of utility
+  //search handler make instance of searcher
   public SearchHandler() {
 
   }
